d2b79fa03aee9703069890b1fedd41f329e206fe
Pratik Bhandari <pratik.bhandari.work@gmail.com>
2024-11-08 18:53:32 +0530

    Coin details screen - initial commit

---
+++ app/src/main/java/com/learn/crypto_tracker/crypto/presentation/coin_details/CoinDetailScreen.kt
@@ -0,0 +1,9 @@
+package com.learn.crypto_tracker.crypto.presentation.coin_details
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun CoinDetailScreen(modifier: Modifier = Modifier) {
+
+}
